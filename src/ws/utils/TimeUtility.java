5a487936d2be14b3d937e28a43e84e73c9e1de44
Arild Wollan <arild-wollan@hotmail.com>
2016-04-22 00:48:19 +0200

    Class for custom timestamps

---
+++ src/ws/utils/TimeUtility.java
@@ -0,0 +1,45 @@
+package ws.utils;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class TimeUtility {
+	
+	public static String getTimeStamp() {
+
+		LocalDate currentDate = LocalDate.now();
+		LocalTime currentTime = LocalTime.now();
+		
+		// Get all desired time values
+		String year = Integer.toString(currentDate.getYear());
+		String month = Integer.toString(currentDate.getMonthValue());
+		String date = Integer.toString(currentDate.getDayOfMonth());
+		
+		String hour = Integer.toString(currentTime.getHour());
+		String minute = Integer.toString(currentTime.getMinute());
+		String second = Integer.toString(currentTime.getSecond());
+		
+		// Format values to the desired format
+		if(month.length() < 2) {
+			month = "0" + month;
+		}
+		
+		if(date.length() < 2) {
+			date = "0" + date;
+		}
+		
+		if(hour.length() < 2) {
+			hour = "0" + hour;
+		}
+		
+		if(minute.length() < 2) {
+			minute = "0" + minute;
+		}
+		
+		if(second.length() < 2) {
+			second = "0" + second;
+		}		
+		
+		return "[" + date + "." + month + "." + year + " - " + hour + ":" + minute + ":" + second + "]";
+	}
+}
